f80602ce2dd8673315a5304a56485a468bca81a1
Indresh Tripathi <indresh.tripathi@alerzo.com>
2022-03-03 14:34:22 +0530

    updating cv

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,13 +1,13 @@
 const data = {};
 
-data.skills = ['Vue', 'ReactJS', 'Angular', 'Webview Development', 'web components',
-    'MongoDB', 'NodeJS', 'Javascript', 'HTML', 'CSS', 'CanJS', 'AWS'];
+data.skills = ['ReactJS', 'Vue', 'Angular', 'Webview Development', 'TypeScript',
+    'PWA', 'NodeJS', 'Javascript', 'HTML', 'CSS', 'GraphQL', 'REST'];
 
-data.objective = `I have 6 years experience in web development seeking Software Engineering work at a company which encourages excellent creative work.`;
+data.objective = `As a software developer with around 7 years of experience my ambition is to combine my knowledge in the fields technology and design. I have a positive attitude and good interpersonal communication skills. With my dedication and enthusiasm I want to develop myself as an IT-specialist in a challenging environment.`;
   
 data.info = {
     name: 'INDRESH TRIPATHI',
-    currentRole: 'SDE 2 @ OLACABS',
+    currentRole: 'Senior Software Engineer @ ALERZO',
     Mobile: ['+917415690395'],
     email: 'indreshsde@gmail.com',
     linkedInURI: 'https://www.linkedin.com/in/indreshtripathi'
@@ -15,24 +15,40 @@ data.info = {
 
 data.projects = [
   {
+    name: 'Veedez web',
+    technology: 'React, Javascript',
+    description: `Web Application for retailers to track inventory, expenses, invoices, businesses and products.`
+  },{
+    name: 'Admin web',
+    technology: 'React, Javascript',
+    description: `Web Application to manage B2B transaction, warehouses, inventories, customers, fleet and products.`
+  },{
+    name: 'Alerzo Shop',
+    technology: 'React, Javascript',
+    description: `Retailer PWA for retail shop owners to order inventory online.`
+  },{
     name: 'OlaMoney Postpaid',
-    technology: 'Javascript, HTML5, Vue.js',
+    technology: 'Vue.js, webview',
     description: `OlaMoney Postpaid is a fast and easy way to pay for your Ola rides and other online services.`
   }, {
     name: 'EMI',
-    technology: 'Javascript, HTML5, Vue.js',
+    technology: 'Vue.js, webview',
     description: 'EMI is a fast and easy way to pay for your purchases on various merchants.'
   }, {
+    name: 'OFS-Console',
+    technology: 'React, Redux, Redux thunk, Javascript',
+    description: 'A web application to manage users onboarding and portal for Product support team.'
+  }, {
     name: 'Asset-Finance-Marketplace',
-    technology: 'Javascript, HTML5, Vue.js',
+    technology: 'Vue.js, webview',
     description: 'Providing complete paperless finance journey for two wheelers and four wheelers vehicles. It has Integration with leading banks. Also integrated with UIDAI and digilocker for smooth kyc verification.'
   }, {
     name: 'OlaMoney External Merchants',
-    technology: 'Javascript, HTML5, Vue.js',
+    technology: 'Vue.js, webview',
     description: 'OlaMoney External Merchants is an integration with merchants for online payments.'
   }, {
     name: 'OlaMoney wallet',
-    technology: 'Javascript, HTML5, Vue.js',
+    technology: 'Vue.js, webview',
     description: `OlaMoney wallet is used to pay for cab fares, mobile bills, DTH recharges and etc.`
   }, {
     name: 'Primavera P6',
@@ -41,7 +57,7 @@ data.projects = [
     It provides a 100% webbased, solution for managing projects of any size.`
   }, {
     name: 'Teamwork Web',
-    technology: 'Javascript, HTML5, CSS 3, Angular 6, Electron',
+    technology: 'Angular 6, Typescript, Electron',
     description: `The basic premise of Teamwork is a virtual gathering place known as a workspace. You can create a workspace for a large team, a small team, or a special project.`
   }, {
     name: 'Connect Client',
@@ -51,23 +67,23 @@ data.projects = [
 ];
 
 data.roles = [{
+  company: 'Alerzo',
+  title: 'Senior Software Engineer',
+  tenure: 'October 2021 - Present',
+  responsibility: `Development of Veedez web app, Alerzo Shop app and admin web app. Building and developing end to end features.
+  Working closely with stakeholders to ensure timely deliveries of product.`
+}, {
   company: 'Olacabs',
   title: 'SDE 2',
-  tenure: 'December 2019 - Present',
-  responsibility: `Delivering production software on schedule.
-  Designing and developing production software with high quality.
-  Work closely with the product owners and architects and understand the business requirements.
-  Understanding all systems components to ensure that one’s software is into the "big picture".
-  Extensively unit test and qualify all code.
-  Contribution in system design and low level architecture.`
+  tenure: 'December 2019 - October 2021',
+  responsibility: `Leading UI team (5 members) of OlaMoney. Working closely with the stakeholders to meet the business requirements.
+  Providing solutions and architecting products. Constantly improving the products by ensuring the best engineering practices to be followed in the team.`
 }, {
   company: 'Oracle India',
   title: 'Application Engineer 2',
   tenure: 'November 2018 - November 2019',
   responsibility: `Developing framework and infrastructure code using current web technologies (HTML5, JavaScript, JQuery).
-  Participating in code reviews.
   Engaging actively in the technical decision making process.
-  Participating in technical discussions and providing feedback depending on the needs of the team.
   Keeping abreast of the latest technologies (including Oracle technologies) and assimilating them into the product suites.
   Pro-actively mentor and keeping track of team mates progress.
   Ensuring compliance with development methodology and technical process.`
@@ -78,9 +94,7 @@ data.roles = [{
   responsibility: `Delivering production software on schedule.
   Designing and developing production software with high quality.
   Work closely with the product owners and architects and understand the business requirements.
-  Understanding all systems components to ensure that one’s software is into the "big picture".
-  Extensively unit test and qualify all code.
-  Contribution in system design and low level architecture.`
+  Understanding all systems components to ensure that one’s software is into the "big picture".`
 }];
 
 data.courses = [{
